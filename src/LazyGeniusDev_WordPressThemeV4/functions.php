0b8eddefa4edcbd44e5f80e096a5d0a459e09ddb
Leon20200809 <kindlyhearted666@gmail.com>
2026-05-04 19:11:10 +0900

    security.phpで基本セキュリティヘッダーを追加

--- src/LazyGeniusDev_WordPressThemeV4/functions.php
+++ src/LazyGeniusDev_WordPressThemeV4/functions.php
@@ -4,6 +4,6 @@ require_once get_theme_file_path('inc/setup.php');
 require_once get_theme_file_path('inc/enqueue.php');
 // 必要に応じて順次オン
 // require_once get_theme_file_path('inc/cleanup.php');
-// require_once get_theme_file_path('inc/security.php');
+require_once get_theme_file_path('inc/security.php');
 // require_once get_theme_file_path('inc/patterns.php');
 // require_once get_theme_file_path('inc/shortcodes.php');
